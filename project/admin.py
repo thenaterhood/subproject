0bce592756802f75c1f923f544db2390a9b86bba
Nate Levesque <public@thenaterhood.com>
2013-12-19 12:24:50 -0500

    register tasks with admin panel

--- project/admin.py
+++ project/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from project.models import *
 
 admin.site.register(Project)
+admin.site.register(ProjectTask)
 admin.site.register(Worklog)
 admin.site.register(ProjectStatistic)
 admin.site.register(UserStatistic)
