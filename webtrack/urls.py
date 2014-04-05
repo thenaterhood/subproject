b343226023583338c6c5604b34ec8c18bc18f8b9
Nate Levesque <public@thenaterhood.com>
2014-04-05 18:44:31 -0400

    add more profile urls to use for things later

--- webtrack/urls.py
+++ webtrack/urls.py
@@ -24,6 +24,8 @@ urlpatterns = patterns('',
     url(r'^user/logout/$', 'user.views.logout'),
     url(r'^accounts/login/$', 'user.views.login_user'),
     url(r'^user/profile/(?P<username>[a-zA-Z0-9_.-]+)/$', 'user.views.view_profile'),
+    url(r'^profile/(?P<username>[a-zA-Z0-9_.-]+)/$', 'user.views.view_profile'),
+    url(r'^u/(?P<username>[a-zA-Z0-9_.-]+)/$', 'user.views.view_profile'),
     url(r'^user/edit/$', 'user.views.edit_profile'),
     url(r'^user/password/$', 'user.views.change_password'),
 
